c3f56af51a98a66aabb53374d62cd58de956a1ad
Joan Lora <joan.lora@outlook.com>
2023-02-03 09:58:48 -0500

    Added a JPA entity in Recipe class

---
+++ src/main/java/com/pluralsight/recipe/models/Recipe.java
@@ -0,0 +1,47 @@
+package com.pluralsight.recipe.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+
+@Entity
+@JsonIgnoreProperties(value = {"hibernateLazyInitializer","handler"})
+public class Recipe {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description", length = Integer.MAX_VALUE)
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe(){
+
+    }
+}
